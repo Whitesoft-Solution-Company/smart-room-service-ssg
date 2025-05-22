dbd725ff2014957d34f8ef976dc2d30caf2a8702
nesjy <63283904+nesjy@users.noreply.github.com>
2025-05-22 16:28:24 +0700

    refactor: token handling and improve verification logic

--- netlify/edge-functions/auth.js
+++ netlify/edge-functions/auth.js
@@ -2,39 +2,39 @@
 export default async (request, context) => {
   const url = new URL(request.url);
 
-  const queryToken = url.searchParams.get("token");
-  const cookieToken = context.cookies.get("authToken")?.value;
-  const token = queryToken || cookieToken;
-  if (!token) {
+  const token = url.searchParams.get("token");
+  const authToken = context.cookies.get("authToken");
+  const tokenVerified = token || authToken;
+  if (!tokenVerified) {
     return Response.redirect("/401", 302);
   }
 
   // Verify token with Strapi
-  const userRes = await fetch(`https://service.ananyalipe.com/api/hotels/${token}`);
+  const userRes = await fetch(`https://service.ananyalipe.com/api/hotels/${tokenVerified}`);
   const userData = await userRes.json(); // Convert response to JSON
-
+   
   // Check if token verification is valid
-  if (!userRes.ok || userData?.data == null) {
+  if (!userRes.ok || userData.data === null) {
       context.cookies.set({
         name: "authToken",
         value: "",
         httpOnly: true,
         path: "/",
-        maxAge: 60 * 60 * 24 * 1
+        maxAge: 60 * 60 * 24 * 1 // 1 day
       });
       return Response.redirect("/401", 302);
   }
 
-  if (queryToken && queryToken !== cookieToken) {
-      context.cookies.set({
-        name: "authToken",
-        value: queryToken,
-        httpOnly: true,
-        path: "/",
-        maxAge: 60 * 60 * 24 * 7
-      });
+  // Set the cookie with the token
+  if(token && token !== authToken) {
+    context.cookies.set({
+      name: "authToken",
+      value: tokenVerified,
+      httpOnly: true,
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7 // 7 days
+    });
   }
-  
 
   if (url.searchParams.get("token")) {
     return Response.redirect("/protected/en/home", 302); // Redirect to homepage with status 302
